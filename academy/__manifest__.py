d6b43b294b4ec1c9a6b62828700ff53cdead35d8
diegom200 <diego@m200la.com>
2022-09-27 14:11:33 +0000

    [ADD] academy1

---
+++ academy/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Oddo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author':'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'], 
+    
+    'data': [
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+    ],
+    'demo': [
+        'demo/academy_demo.xml',
+        
+    ],
+}
